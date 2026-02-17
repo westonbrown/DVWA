d434758a758ca5612816abad02fa3a13273e7d28
Aaron Brown <47581657+westonbrown@users.noreply.github.com>
2026-02-17 15:32:39 +0000

    Fix path traversal in vulnerabilities/view_source_all.php (validate id, realpath checks)

--- vulnerabilities/view_source_all.php
+++ vulnerabilities/view_source_all.php
@@ -8,24 +8,47 @@ dvwaPageStartup(array('authenticated'));
 $page = dvwaPageNewGrab();
 $page['title'] = 'Source' . $page['title_separator'] . $page['title'];
 
-if (array_key_exists("id", $_GET)) {
+if( array_key_exists( 'id', $_GET ) ) {
 	$id = $_GET['id'];
 
-	$lowsrc = @file_get_contents("./{$id}/source/low.php");
-	$lowsrc = str_replace(array('$html .='), array('echo'), $lowsrc);
-	$lowsrc = highlight_string($lowsrc, true);
+	// Prevent traversal/arbitrary file reads
+	if( preg_match( '/^[a-z0-9_]+$/', $id ) !== 1 ) {
+		$page['body'] = "<p>Not found</p>";
+		dvwaSourceHtmlEcho($page);
+		exit;
+	}
 
-	$medsrc = @file_get_contents("./{$id}/source/medium.php");
-	$medsrc = str_replace(array('$html .='), array('echo'), $medsrc);
-	$medsrc = highlight_string($medsrc, true);
+	$baseDir = realpath( __DIR__ . DIRECTORY_SEPARATOR . $id . DIRECTORY_SEPARATOR . 'source' );
+	if( $baseDir === false ) {
+		$page['body'] = "<p>Not found</p>";
+		dvwaSourceHtmlEcho($page);
+		exit;
+	}
 
-	$highsrc = @file_get_contents("./{$id}/source/high.php");
+	function dvwa_safe_read_source( $baseDir, $filename ) {
+		$candidate = $baseDir . DIRECTORY_SEPARATOR . $filename;
+		$resolved  = realpath( $candidate );
+		if( $resolved === false || strpos( $resolved, $baseDir ) !== 0 || !is_file( $resolved ) ) {
+			return '';
+		}
+		return @file_get_contents( $resolved );
+	}
+
+	$lowsrc  = dvwa_safe_read_source( $baseDir, 'low.php' );
+	$lowsrc  = str_replace(array('$html .='), array('echo'), $lowsrc);
+	$lowsrc  = highlight_string($lowsrc, true);
+
+	$medsrc  = dvwa_safe_read_source( $baseDir, 'medium.php' );
+	$medsrc  = str_replace(array('$html .='), array('echo'), $medsrc);
+	$medsrc  = highlight_string($medsrc, true);
+
+	$highsrc = dvwa_safe_read_source( $baseDir, 'high.php' );
 	$highsrc = str_replace(array('$html .='), array('echo'), $highsrc);
 	$highsrc = highlight_string($highsrc, true);
 
-	$impsrc = @file_get_contents("./{$id}/source/impossible.php");
-	$impsrc = str_replace(array('$html .='), array('echo'), $impsrc);
-	$impsrc = highlight_string($impsrc, true);
+	$impsrc  = dvwa_safe_read_source( $baseDir, 'impossible.php' );
+	$impsrc  = str_replace(array('$html .='), array('echo'), $impsrc);
+	$impsrc  = highlight_string($impsrc, true);
 
 	switch ($id) {
 		case "javascript":
@@ -116,7 +139,8 @@ if (array_key_exists("id", $_GET)) {
 		</form>
 
 	</div>\n";
-} else {
+}
+else {
 	$page['body'] = "<p>Not found</p>";
 }
 
