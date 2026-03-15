eca024c415e92100126ab31d2f4981f7bea749c4
Aaron Brown <westonbrown@users.noreply.github.com>
2026-03-14 21:11:38 -0500

    fix(api): remove hardcoded token/encryption secrets and require env-managed keys

--- vulnerabilities/api/src/Login.php
+++ vulnerabilities/api/src/Login.php
@@ -7,16 +7,30 @@ use OpenApi\Attributes as OAT;
 class Login
 {
 	private const ACCESS_TOKEN_LIFE = 180;
-	private const ACCESS_TOKEN_SECRET = "12345";
 	private const REFRESH_TOKEN_LIFE = 240;
-	private const REFRESH_TOKEN_SECRET = "98765";
+
+	private static function getAccessTokenSecret(): string {
+		$secret = getenv('DVWA_ACCESS_TOKEN_SECRET') ?: '';
+		if (strlen($secret) < 32) {
+			throw new \RuntimeException('DVWA_ACCESS_TOKEN_SECRET must be set to a 32+ character value');
+		}
+		return $secret;
+	}
+
+	private static function getRefreshTokenSecret(): string {
+		$secret = getenv('DVWA_REFRESH_TOKEN_SECRET') ?: '';
+		if (strlen($secret) < 32) {
+			throw new \RuntimeException('DVWA_REFRESH_TOKEN_SECRET must be set to a 32+ character value');
+		}
+		return $secret;
+	}
 	
 	public static function create_token() {
 		$now = time();
 		$tokenObj = new Token();
 		$token = json_encode (array (
-			"access_token" => $tokenObj->create_token(self::ACCESS_TOKEN_SECRET, $now + self::ACCESS_TOKEN_LIFE),
-			"refresh_token" => $tokenObj->create_token(self::REFRESH_TOKEN_SECRET, $now + self::REFRESH_TOKEN_LIFE),
+			"access_token" => $tokenObj->create_token(self::getAccessTokenSecret(), $now + self::ACCESS_TOKEN_LIFE),
+			"refresh_token" => $tokenObj->create_token(self::getRefreshTokenSecret(), $now + self::REFRESH_TOKEN_LIFE),
 			"token_type" => "bearer",
 			"expires_in" => self::ACCESS_TOKEN_LIFE)
 		);
@@ -30,7 +44,7 @@ class Login
 		if ($decrypted === false) {
 			return false;
 		}
-		if ($decrypted['secret'] == self::ACCESS_TOKEN_SECRET && $decrypted['expires'] > time()) {
+		if ($decrypted['secret'] == self::getAccessTokenSecret() && $decrypted['expires'] > time()) {
 			return true;
 		}
 		return false;
@@ -40,7 +54,7 @@ class Login
 		$tokenObj = new Token();
 		$decrypted = $tokenObj->decrypt_token ($token);
 
-		if ($decrypted['secret'] == self::REFRESH_TOKEN_SECRET && $decrypted['expires'] > time()) {
+		if ($decrypted['secret'] == self::getRefreshTokenSecret() && $decrypted['expires'] > time()) {
 			return true;
 		}
 		return false;
--- vulnerabilities/api/src/Token.php
+++ vulnerabilities/api/src/Token.php
@@ -7,7 +7,6 @@ use OpenApi\Attributes as OAT;
 #[OAT\Schema(required: ['token'])]
 class Token {
 	private const ENCRYPTION_CIPHER = "aes-128-gcm";
-	private const ENCRYPTION_KEY = "Paintbrush";
 
     # Not sure if this is needed
     #[OAT\Property(example: "11111")]
@@ -19,10 +18,18 @@ class Token {
 	public function __construct () {
 	}
 
+	private static function getEncryptionKey(): string {
+		$key = getenv('DVWA_TOKEN_ENCRYPTION_KEY') ?: '';
+		if (strlen($key) < 32) {
+			throw new \RuntimeException('DVWA_TOKEN_ENCRYPTION_KEY must be set to a 32+ character value');
+		}
+		return substr(hash('sha256', $key, true), 0, 16);
+	}
+
 	private static function encrypt($cleartext) {
 		$ivlen = openssl_cipher_iv_length(self::ENCRYPTION_CIPHER);
 		$iv = openssl_random_pseudo_bytes($ivlen);
-		$ciphertext = openssl_encrypt($cleartext, self::ENCRYPTION_CIPHER, self::ENCRYPTION_KEY, $options=0, $iv, $tag);
+		$ciphertext = openssl_encrypt($cleartext, self::ENCRYPTION_CIPHER, self::getEncryptionKey(), $options=0, $iv, $tag);
 		$ret = base64_encode ($tag . ":::::" . $iv . ":::::" . $ciphertext);
 		return $ret;
 	}
@@ -36,7 +43,7 @@ class Token {
 		$value = $bits[2];
 		$iv = $bits[1];
 		$tag = $bits[0];
-		$cleartext = openssl_decrypt($value, self::ENCRYPTION_CIPHER, self::ENCRYPTION_KEY, $options=0, $iv, $tag);
+		$cleartext = openssl_decrypt($value, self::ENCRYPTION_CIPHER, self::getEncryptionKey(), $options=0, $iv, $tag);
 		return $cleartext;
 	}
 	public function create_token($secret, $expires) {
